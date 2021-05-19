205caef4887b7590ae0a196221909bab01e72c5b
Kirill Ki <kirillkalapov@gmail.com>
2021-05-19 13:03:46 +0200

    delay change opacity

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -24,6 +24,12 @@ class ViewController: UIViewController {
         
         //print(sender.currentTitle)
         playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+        }
+        
     }
     
     func playSound(soundName: String) {
